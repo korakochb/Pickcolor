a56e9fa51490e642b1af4c42e75747afa68def02
kiruy <prince0908@hotmail.co.th>
2019-11-22 21:47:29 +0700

    Edit controller and model

--- app/src/main/java/com/example/pickcolor/Model.java
+++ app/src/main/java/com/example/pickcolor/Model.java
@@ -6,20 +6,15 @@ import android.graphics.Color;
 
 public class Model  {
 
-    private String hex;
     private int red;
     private int green;
     private int blue;
+    private int x;
+    private int y;
+    private int pixel;
+    private String color;
 
     public Model() {
-    }
-
-    public String getHex() {
-        return hex;
-    }
-
-    public void setHex(String hex) {
-        this.hex = hex;
     }
 
     public int getRed() {
@@ -44,6 +39,38 @@ public class Model  {
 
     public void setBlue(int blue) {
         this.blue = blue;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPixel() {
+        return pixel;
+    }
+
+    public void setPixel(int pixel) {
+        this.pixel = pixel;
     }
 
     public String getComplementaryColor(int colorToInvert) {
